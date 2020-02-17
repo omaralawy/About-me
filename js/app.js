87438bb99598234bdd1701675e63b87326953edd
omar <omaralawii26@gmail.com>
2020-02-18 00:01:21 +0200

    new cool things

---
+++ js/app.js
@@ -0,0 +1,71 @@
+'use strict'
+var clint = prompt("what is your name");
+switch (clint) {
+    case 'Baraah':
+        alert("you are the best");
+        console.log('you are the best !');
+        break;
+    case 'shihab':
+        alert("I'm inspired by you");
+        console.log('im inspired by you');
+        break;
+    case 'Baraa':
+        alert('You are really something special');
+        console.log('im inspired by you');
+        break;
+}
+var name = prompt("my nickname is otar, yes or no ?").toLowerCase();
+switch (name) {
+    case 'yes':
+        alert("GJ !");
+        console.log('GJ !');
+        break;
+    case 'no':
+        alert("wrong ");
+        console.log('wrong');
+        break;
+}
+var game = prompt("i play pubg, yes or no ?").toLowerCase();
+switch (game) {
+    case 'yes':
+        alert("no not all the gamer play pubg :p");
+        console.log('wronge');
+        break;
+    case 'no':
+        alert("wow");
+        console.log('wow');
+        break;
+}
+var age = prompt("i am 20 ?").toLowerCase();
+switch (age) {
+    case 'yes':
+        alert("noooooo");
+        console.log('wrong');
+        break;
+    case 'no':
+        alert("GoodJob");
+        console.log('GoodJob');
+        break;
+}
+var smartphone = prompt("i use samsung ?").toLowerCase();
+switch (smartphone) {
+    case 'yes':
+        alert("indeed");
+        console.log('indeed');
+        break;
+    case 'no':
+        alert("wrong");
+        console.log('wrong');
+        break;
+}
+var opinion = prompt("you are amazin9 ?").toLowerCase();
+switch (age) {
+    case 'yes':
+        alert("yess");
+        console.log('yess');
+        break;
+    case 'no':
+        alert("wrongg");
+        console.log('wrong');
+        break;
+}
